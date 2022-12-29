689687dd8acee78f22607c2805cc262e99604952
NguyenDiem <diem.nguyen@lotussoft.vn>
2022-12-29 15:23:05 +0700

    Update liquidation logic

--- contracts/PrimaryIndexTokenLiquidation.sol
+++ contracts/PrimaryIndexTokenLiquidation.sol
@@ -16,8 +16,16 @@ contract PrimaryIndexTokenLiquidation is Initializable, AccessControlUpgradeable
     bytes32 public constant MODERATOR_ROLE = keccak256("MODERATOR_ROLE");
     uint256 public constant LIQUIDATOR_REWARD_FACTOR_DECIMAL = 18;
     uint256 public minPartialLiquidationAmount;
+    Ratio public targetHealthFactor;
+    Ratio public liquidatorRewardCalcFactor;
+    Ratio public maxLRF;
 
     IPrimaryIndexToken public primaryIndexToken;
+
+    struct Ratio {
+        uint8 numerator;
+        uint8 denominator;
+    }
 
     event Liquidate(address indexed liquidator, address indexed borrower, address lendingToken, address indexed prjAddress, uint256 amountPrjLiquidated);
 
@@ -52,23 +60,51 @@ contract PrimaryIndexTokenLiquidation is Initializable, AccessControlUpgradeable
         minPartialLiquidationAmount = _amount;
     }
 
+    function setMaxLRF(uint8 numeratorLRF, uint8 denominatorLRF) external onlyModerator() {
+        maxLRF = Ratio(numeratorLRF, denominatorLRF);
+    }
+
+    function setLiquidatorRewardCalculationFactor(uint8 numeratorLRF, uint8 denominatorLRF) external onlyModerator() {
+        liquidatorRewardCalcFactor = Ratio(numeratorLRF, denominatorLRF);
+    }
+
     function setPrimaryIndexTokenAddress(address _newPrimaryIndexToken) external onlyModerator() {
         primaryIndexToken = IPrimaryIndexToken(_newPrimaryIndexToken);
     }
 
-    function liquidate(address _account, address _projectToken, address _lendingToken, bool _isPartial, uint256 _lendingTokenAmount) public isProjectTokenListed(_projectToken) isLendingTokenListed(_lendingToken) {
+    function setTargetHealthFactor(uint8 numeratorHF, uint8 denominatorHF) external onlyModerator {
+        targetHealthFactor = Ratio(numeratorHF, denominatorHF);
+    }
+
+    function liquidate(address _account, address _projectToken, address _lendingToken, uint256 _lendingTokenAmount) public isProjectTokenListed(_projectToken) isLendingTokenListed(_lendingToken) {
+        uint minLA = getMinLiquidatorRewardAmount(_account, _projectToken, _lendingToken);
+        uint maxLA = getMaxLiquidatorRewardAmount(_account, _projectToken, _lendingToken);
+        // uint collateralReduce = primaryIndexToken.getTokenEvaluation(_projectToken, _lendingTokenAmount) * lrfNumerator / lrfDenominator;
+        require(_lendingTokenAmount >= minLA && _lendingTokenAmount <= maxLA, "PIT: invalid amount");
+        
         primaryIndexToken.updateInterestInBorrowPositions(_account, _lendingToken);
+
+        uint256 projectTokenToSendToLiquidator = getProjectTokenToSendToLiquidator(_account, _projectToken, _lendingToken,_lendingTokenAmount);
+
+        distributeReward(_account, _projectToken, projectTokenToSendToLiquidator);
+
+        emit Liquidate(msg.sender, _account, _lendingToken, _projectToken, projectTokenToSendToLiquidator);
+    }
+
+    function getProjectTokenToSendToLiquidator(address _account, address _projectToken, address _lendingToken, uint256 _lendingTokenAmount) internal returns(uint256 projectTokenToSendToLiquidator) {
         (uint256 healthFactorNumerator, uint256 healthFactorDenominator) = primaryIndexToken.healthFactor(_account, _projectToken, _lendingToken);
         if(healthFactorNumerator >= healthFactorDenominator){
             revert("PIT: healthFactor>=1");
         }
         uint256 projectTokenMultiplier = 10 ** ERC20Upgradeable(_projectToken).decimals();
-        uint256 lendingTokenAmountToRepay = _isPartial ? _lendingTokenAmount : type(uint256).max;
-        uint256 repaid = primaryIndexToken.repay(_projectToken, _lendingToken, lendingTokenAmountToRepay, msg.sender, _account);
+        uint256 repaid = primaryIndexToken.repay(_projectToken, _lendingToken, _lendingTokenAmount, msg.sender, _account);
         uint256 projectTokenEvaluation = repaid * projectTokenMultiplier / primaryIndexToken.getTokenEvaluation(_projectToken, projectTokenMultiplier);
-        (uint256 lrfNumerator, uint256 lrfDenominator) = getLiquidatorRewardFactor(_account, _projectToken, _lendingToken, _isPartial);
-        uint256 projectTokenToSendToLiquidator = projectTokenEvaluation * lrfNumerator / lrfDenominator;
-        
+
+        (uint256 lrfNumerator, uint256 lrfDenominator) = liquidatorRewardFactor(_account, _projectToken, _lendingToken);
+        projectTokenToSendToLiquidator = projectTokenEvaluation * lrfNumerator / lrfDenominator;
+    }
+
+    function distributeReward(address _account, address _projectToken, uint256 projectTokenToSendToLiquidator) internal {
         uint256 depositedProjectTokenAmount = primaryIndexToken.getDepositedAmount(_projectToken, _account);
         if(projectTokenToSendToLiquidator > depositedProjectTokenAmount){
             projectTokenToSendToLiquidator = depositedProjectTokenAmount;
@@ -77,37 +113,43 @@ contract PrimaryIndexTokenLiquidation is Initializable, AccessControlUpgradeable
         primaryIndexToken.setDepositedPosition(_account, _projectToken, depositedProjectTokenAmount - projectTokenToSendToLiquidator);
         primaryIndexToken.setTotalDepositedProjectToken(_projectToken, totalDepositedProjectToken - projectTokenToSendToLiquidator);
         primaryIndexToken.distributeReward(msg.sender, _projectToken, projectTokenToSendToLiquidator);
-
-        emit Liquidate(msg.sender, _account, _lendingToken, _projectToken, projectTokenToSendToLiquidator);
     }
 
-    function getLiquidatorRewardFactor(address _account, address _projectToken, address _lendingToken, bool _isPartial) public view returns (uint256 numerator, uint256 denominator) {
-        if (_isPartial) {
-            // handle getLiquidatorRewardFactor for partial liquidation
-            numerator = primaryIndexToken.projectTokenInfo(_projectToken).liquidationIncentive.numerator;
-            denominator = primaryIndexToken.projectTokenInfo(_projectToken).liquidationIncentive.denominator;
-        } else {
-            numerator = primaryIndexToken.projectTokenInfo(_projectToken).liquidationIncentive.numerator;
-            denominator = primaryIndexToken.projectTokenInfo(_projectToken).liquidationIncentive.denominator;
-        }
+    function liquidatorRewardFactor(address _account, address _projectToken, address _lendingToken) public view returns(uint256 lrfNumerator, uint256 lrfDenominator) {
+        (uint256 hfNumerator, uint256 hfDenominator) = primaryIndexToken.healthFactor(_account, _projectToken, _lendingToken);
+        Ratio memory kf = liquidatorRewardCalcFactor;
+        lrfNumerator = kf.numerator * (hfDenominator - hfNumerator) + kf.denominator * hfDenominator;
+        lrfDenominator = kf.denominator * hfDenominator;
+        uint lrfNumeratorMul = lrfNumerator * maxLRF.denominator;
+        uint maxLRFNumerator = maxLRF.numerator * lrfDenominator;
+        if(lrfNumeratorMul > maxLRFNumerator){
+            (lrfNumerator, lrfDenominator) = (maxLRF.numerator, maxLRF.denominator);
+        }  
     }
-
+    
     // MaxLA = (LVR * CVc - THF * LVc) / (LRF * LVR - THF)
-    function getMaxLiquidatorRewardFactor(address _account, address _projectToken, address _lendingToken, uint256 _numeratorTargetHealthFactor, uint256 _denominatorTargetHealthFactor) public view returns (uint256) {
-        (uint256 lrfNumerator, uint256 lrfDenominator) = getLiquidatorRewardFactor(_account, _projectToken, _lendingToken, true);
+    function getMaxLiquidatorRewardAmount(address _account, address _projectToken, address _lendingToken) public view returns (uint256 maxLA) {
+        (uint256 lrfNumerator, uint256 lrfDenominator) = liquidatorRewardFactor(_account, _projectToken, _lendingToken);
+        Ratio memory targetHf = targetHealthFactor;
+
         uint256 lvrNumerator = primaryIndexToken.projectTokenInfo(_projectToken).liquidationIncentive.numerator;
         uint256 lvrDenominator = primaryIndexToken.projectTokenInfo(_projectToken).liquidationIncentive.denominator;
+
         uint256 totalOutstandingInUSD = primaryIndexToken.totalOutstandingInUSD(_account, _projectToken, _lendingToken);
+
         uint256 depositedProjectTokenAmount = primaryIndexToken.getDepositedAmount(_projectToken, _account);
         uint256 depositedProjectTokenAmountInUSD = primaryIndexToken.getTokenEvaluation(_projectToken, depositedProjectTokenAmount);
+
+        uint256 numeratorMaxLA = (lvrNumerator * depositedProjectTokenAmountInUSD * targetHf.denominator - lvrDenominator * targetHf.numerator * totalOutstandingInUSD) * lrfDenominator;
+        uint256 denominatorMaxLA = lrfNumerator * lvrNumerator * targetHf.denominator - targetHf.numerator * lvrDenominator * lrfDenominator;
+        uint256 calculatedMaxLA = numeratorMaxLA * 10 ** LIQUIDATOR_REWARD_FACTOR_DECIMAL / denominatorMaxLA;
         
-        uint256 numeratorMaxLA = (lvrNumerator * depositedProjectTokenAmountInUSD * _denominatorTargetHealthFactor - lvrDenominator * _numeratorTargetHealthFactor * totalOutstandingInUSD) * lrfDenominator;
-        uint256 denominatorMaxLA = lrfNumerator * lvrNumerator * _denominatorTargetHealthFactor - _numeratorTargetHealthFactor * lvrDenominator * lrfDenominator;
-        return numeratorMaxLA * 10 ** LIQUIDATOR_REWARD_FACTOR_DECIMAL / denominatorMaxLA;
+        maxLA = calculatedMaxLA > totalOutstandingInUSD ? totalOutstandingInUSD : calculatedMaxLA;
     }
 
-    function getMinLiquidatorRewardFactor(address _account, address _projectToken, address _lendingToken, uint256 _numeratorTargetHealthFactor, uint256 _denominatorTargetHealthFactor) public view returns (uint256) {
-        uint256 maxLrf = getMaxLiquidatorRewardFactor(_account, _projectToken, _lendingToken, _numeratorTargetHealthFactor, _denominatorTargetHealthFactor);
+    //MinLA = min(MaxLA, MPA)
+    function getMinLiquidatorRewardAmount(address _account, address _projectToken, address _lendingToken) public view returns (uint256) {
+        uint256 maxLrf = getMaxLiquidatorRewardAmount(_account, _projectToken, _lendingToken);
         return Math.min(maxLrf, minPartialLiquidationAmount * 10 ** LIQUIDATOR_REWARD_FACTOR_DECIMAL);
     }
 }
